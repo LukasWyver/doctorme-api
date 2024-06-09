d102cfefdb70e392c593ecfdfd26abb947a15966
lukaswyver <lucas_sin5@hotmail.com>
2024-06-09 19:17:40 -0300

    feat: add use case get doctor by id

--- src/infra/controller/DoctorController.ts
+++ src/infra/controller/DoctorController.ts
@@ -2,11 +2,18 @@ import { Request, Response } from "express";
 import { database } from "@/infra/DatabaseService";
 import DoctorController from "@/application/controller/DoctorController";
 import ListDoctorUseCase from "@/application/use-cases/doctor/ListDoctor";
+import GetDoctorByIdUseCase from "@/application/use-cases/doctor/GetDoctorById";
 
 export default class DoctorControllerImpl implements DoctorController {
   async listDoctor(req: Request, res: Response) {
     const useCase = new ListDoctorUseCase(database)
     const doctors = await useCase.execute()
     res.status(200).json(doctors)
+  }
+
+  async getDoctorById(req: Request, res: Response){
+    const useCase = new GetDoctorByIdUseCase(database)
+    const doctor = await useCase.execute(Number(req.params.id))
+    res.status(200).json(doctor)
   }
 }
