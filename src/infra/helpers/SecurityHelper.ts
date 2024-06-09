a8bf0701f5d2871beb9f910c6902db672d5f9b38
lukaswyver <lucas_sin5@hotmail.com>
2024-06-09 19:15:14 -0300

    feat: add functions encode and decode from base 64

--- src/infra/helpers/SecurityHelper.ts
+++ src/infra/helpers/SecurityHelper.ts
@@ -8,3 +8,13 @@ export function hashPassword(password: string){
 export function comparePassword(password: string, hash: string){
   return bcrypt.compareSync(password, hash)
 }
+
+// Função para codificar uma string para Base64
+export function encodeToBase64(data: string){
+  return Buffer.from(data, 'utf-8').toString('base64');
+}
+
+// Função para decodificar uma string de Base64 para UTF-8
+export function decodeFromBase64(data: string){
+  return Buffer.from(data, 'base64').toString('utf-8');
+}
